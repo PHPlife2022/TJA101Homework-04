e8e514502ea33db59c01afd6c6abecea9bf222eb
Z.X.Tsai <classroom078087@gmail.com>
2025-04-12 09:25:38 +0800

    re

--- src/Homework5/Homework5_3_MyRectangle.java
+++ src/Homework5/Homework5_3_MyRectangle.java
@@ -1,15 +1,21 @@
 package Homework5;
 
 public class Homework5_3_MyRectangle {
-	double width;
-	double depth;
-	void setWidth(double width) {
+	private double width;
+	private double depth;
+	public void setWidth(double width) {
 		this.width = width;
 	}
-	void setDepth(double depth) {
+	public double getWidth() {
+		return width;
+	}
+	public void setDepth(double depth) {
 		this.depth = depth;		
 	}
-	double getArea() {
+	public double getDepth() {
+		return depth;
+	}
+	public double getArea() {
 		return (width * depth);
 	}
 	public Homework5_3_MyRectangle() {		
@@ -19,9 +25,3 @@ public class Homework5_3_MyRectangle {
 		this.depth = depth;
 	}
 };
-class MyRectangleMain{
-	public static void main(String[] args) {
-		Homework5_3_MyRectangle mr = new Homework5_3_MyRectangle(10,20);
-		System.out.println(mr.getArea());
-	}
-}
---
+++ src/Homework5/MyRectangleMain.java
@@ -0,0 +1,9 @@
+package Homework5;
+
+public class MyRectangleMain {	
+	public static void main(String[] args) {		
+		Homework5_3_MyRectangle mr = new Homework5_3_MyRectangle(10,20);
+		System.out.println(mr.getArea());		
+	}
+
+}
